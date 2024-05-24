e5f8049b9a112f9a92955906c5187fa5fea7a6b3
John O'Brien <johnobrien.dev@gmail.com>
2024-05-24 14:31:58 -0400

    create disc model

---
+++ models/disc.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const manufacturer = require("./manufacturer");
+const Schema = mongoose.Schema;
+
+const DiscSchema = new Schema({
+  title: {type: String, required : true},
+  manufacturer: {type: Schema.Types.ObjectId, ref: "Manufacturer", required: true},
+  disctype: {type: Schema.Types.ObjectId, ref: "Disctype", required: true, enum: ["Distance", "Fairway", "Midrange", "Putter"]},
+  speed: {type: Number, required: true, minLength: 1, maxLength: 14},
+  glide: {type: Number, required: true, minLength: 1, maxLength: 7},
+  turn: {type: Number, required: true, minLength: -4, maxLength: 1},
+  fade: {type: Number, required: true, minLength: 0, maxLength: 5},
+});
+
+DiscSchema.virtual("url").get(function () {
+  return `/disc/${this._id}`
+})
+
+module.exports = mongoose.model("Disc", DiscSchema);
