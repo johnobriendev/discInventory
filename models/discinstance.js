2e45926f75e814161d3fa914307701dd6d1f9075
John O'Brien <johnobrien.dev@gmail.com>
2024-05-27 19:10:20 -0400

    finish populatedb

--- models/discinstance.js
+++ models/discinstance.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const DiscinstanceSchema = new Schema({
   plastic: {type: String, required: true, enum: ["DX", "Pro", "Champion", "Star", "GStar", "Halo Star"]},
-  weight: {type: Number, required: true, minLength: 130, maxLength: 180}
+  weight: {type: Number, required: true, minLength: 130, maxLength: 180},
+  color: {type: String, required: true,},
 })
 
 DiscinstanceSchema.virtual("url").get(function () {
--- populatedb.js
+++ populatedb.js
@@ -45,11 +45,10 @@ async function disctypeCreate(index, name) {
 }
 
 async function manufacturerCreate(index, name) {
-  const authordetail = { name: name };
-  if (d_birth != false) authordetail.date_of_birth = d_birth;
-  if (d_death != false) authordetail.date_of_death = d_death;
+  const manufacturerdetail = { name: name };
+  
 
-  const manufacturer = new Manufacturer(authordetail);
+  const manufacturer = new Manufacturer(manufacturerdetail);
 
   await manufacturer.save();
   manufacturers[index] = manufacturer;
@@ -73,139 +72,108 @@ async function discCreate(index, name, manufacturer, disctype, speed, glide, tur
   console.log(`Added disc: ${name}`);
 }
 
-async function discinstanceCreate(index, disc, instance, plastic, weight) {
+async function discinstanceCreate(index, disc, plastic, weight, color) {
   const discinstancedetail = {
     disc: disc,
-    instance: instance,
     plastic: plastic,
     weight: weight,
+    color: color,
   };
  
 
   const discinstance = new DiscInstance(discinstancedetail);
   await discinstance.save();
   discinstances[index] = discinstance;
-  console.log(`Added discinstance: ${imprint}`);
+  console.log(`Added discinstance: ${instance}`);
 }
 
-async function createGenres() {
-  console.log("Adding genres");
+async function createDisctypes() {
+  console.log("Adding disctypes");
   await Promise.all([
-    genreCreate(0, "Fantasy"),
-    genreCreate(1, "Science Fiction"),
-    genreCreate(2, "French Poetry"),
+    disctypeCreate(0, "Distance Driver"),
+    disctypeCreate(1, "Fairway Driver"),
+    disctypeCreate(2, "Midrange"),
+    disctypeCreate(3, "Putter"),
   ]);
 }
 
-async function createAuthors() {
-  console.log("Adding authors");
+async function createManufacturers() {
+  console.log("Adding manufacturers");
   await Promise.all([
-    authorCreate(0, "Patrick", "Rothfuss", "1973-06-06", false),
-    authorCreate(1, "Ben", "Bova", "1932-11-8", false),
-    authorCreate(2, "Isaac", "Asimov", "1920-01-02", "1992-04-06"),
-    authorCreate(3, "Bob", "Billings", false, false),
-    authorCreate(4, "Jim", "Jones", "1971-12-16", false),
+    manufacturerCreate(0, "Innova"),
+    manufacturerCreate(1, "Millennium" ),
+    manufacturerCreate(2, "Discraft"),
+    manufacturerCreate(3, "Discmania"),
+    manufacturerCreate(4, "Axiom"),
   ]);
 }
 
-async function createBooks() {
-  console.log("Adding Books");
+async function createDiscs() {
+  console.log("Adding Discs ");
   await Promise.all([
-    bookCreate(0,
-      "The Name of the Wind (The Kingkiller Chronicle, #1)",
-      "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
-      "9781473211896",
-      authors[0],
-      [genres[0]]
-    ),
-    bookCreate(1,
-      "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-      "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-      "9788401352836",
-      authors[0],
-      [genres[0]]
-    ),
-    bookCreate(2,
-      "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-      "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-      "9780756411336",
-      authors[0],
-      [genres[0]]
-    ),
-    bookCreate(3,
-      "Apes and Angels",
-      "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-      "9780765379528",
-      authors[1],
-      [genres[1]]
-    ),
-    bookCreate(4,
-      "Death Wave",
-      "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-      "9780765379504",
-      authors[1],
-      [genres[1]]
-    ),
-    bookCreate(5,
-      "Test Book 1",
-      "Summary of test book 1",
-      "ISBN111111",
-      authors[4],
-      [genres[0], genres[1]]
-    ),
-    bookCreate(6,
-      "Test Book 2",
-      "Summary of test book 2",
-      "ISBN222222",
-      authors[4],
-      false
-    ),
+    discCreate(0, "Destroyer", "Innova", "Distance Driver", 12, 5, -1, 3),
+    discCreate(1, "Wraith", "Innova", "Distance Driver", 11, 5, -1, 3),
+    discCreate(2, "Beast", "Innova", "Distance Driver", 10, 5, -2, 2),
+    discCreate(3, "Sidewinder", "Innova", "Distance Driver", 9, 5, -3, 1),
+    discCreate(4, "Tern", "Innova", "Distance Driver", 12, 5, -3, 2),
+    discCreate(5, "Teebird", "Innova", "Fairway Driver", 7, 5, 0, 2),
+    discCreate(6, "Eagle", "Innova", "Fairway Driver", 7, 5, -1, 3),
+    discCreate(7, "Leopard", "Innova", "Fairway Driver", 6, 5, -2, 1),
+    discCreate(8, "Roc", "Innova", "Midrange", 4, 4, 0, 3),
+    discCreate(9, "Shark", "Innova", "Midrange", 4, 4, 0, 2),
+    discCreate(10, "Aviar", "Innova", "Midrange", 2, 3, 0, 1),
+    discCreate(11, "KC Pro Aviar", "Innova", "Midrange", 2, 3, 0, 2),
+    
   ]);
 }
 
-async function createBookInstances() {
-  console.log("Adding authors");
+async function createDiscinstances() {
+  console.log("Adding discinstances");
   await Promise.all([
-    bookInstanceCreate(0, books[0], "London Gollancz, 2014.", false, "Available"),
-    bookInstanceCreate(1, books[1], " Gollancz, 2011.", false, "Loaned"),
-    bookInstanceCreate(2, books[2], " Gollancz, 2015.", false, false),
-    bookInstanceCreate(3,
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available"
+    discinstanceCreate(0, discs[0], "Star", 175, "Red"),
+    discinstanceCreate(1, discs[1], "GStar", 172, "Yellow"),
+    discinstanceCreate(2, discs[2], "Pro", 175, "Purple"),
+    discinstanceCreate(3,
+      discs[3],
+      "Halo Star",
+      169,
+      "Pink"
     ),
-    bookInstanceCreate(4,
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available"
+    discinstanceCreate(4,
+      discs[3],
+      "GStar",
+      165,
+      "Light Blue"
     ),
-    bookInstanceCreate(5,
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available"
+    discinstanceCreate(5,
+      discs[3],
+      "Star",
+      175,
+      "Blue"
     ),
-    bookInstanceCreate(6,
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Available"
+    discinstanceCreate(6,
+      discs[4],
+      "Champion",
+      165,
+      "Orange"
     ),
-    bookInstanceCreate(7,
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Maintenance"
+    discinstanceCreate(7,
+      discs[4],
+      "Halo Star",
+      170,
+      "Clear"
     ),
-    bookInstanceCreate(8,
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Loaned"
+    discinstanceCreate(8,
+      discs[5],
+      "Champion",
+      175,
+      "Pink"
     ),
-    bookInstanceCreate(9, books[0], "Imprint XXX2", false, false),
-    bookInstanceCreate(10, books[1], "Imprint XXX3", false, false),
+    discinstanceCreate(9, discs[6], "GStar", 175, "Blue"),
+    discinstanceCreate(10, discs[7], "Star", 175, "Green"),
+    discinstanceCreate(0, discs[8], "DX", 175, "Red"),
+    discinstanceCreate(0, discs[9], "DX", 175, "Red"),
+    discinstanceCreate(0, discs[10], "Pro", 175, "Red"),
+    discinstanceCreate(0, discs[10], "Pro", 175, "Red"),
   ]);
 }
