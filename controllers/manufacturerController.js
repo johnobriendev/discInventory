aa7c212bfe80a8f9e1382d809330a8f474553d68
John O'Brien <johnobrien.dev@gmail.com>
2024-05-27 21:50:18 -0400

    create manufacturer controller

---
+++ controllers/manufacturerController.js
@@ -0,0 +1,35 @@
+const Manufacturer = require("..models/manufacturer");
+const asyncHandler = require("express-async-handler");
+// Display list of all Manufacturers.
+exports.manufacturer_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Manufacturer list");
+});
+
+// Display detail page for a specific Manufacturer.
+exports.manufacturer_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+});
+//Display manufacturer Create on get
+exports.manufacturer_create_get.asyncHandler(async(req, res, next) =>{
+  res.send("not implemented");
+});
+// create post
+exports.manufacturer_create_post.asyncHandler(async(req, res, next) =>{
+  res.send("not implemented");
+});
+//delete get
+exports.manufacturer_delete_get.asyncHandler(async(req, res, next) =>{
+  res.send("not implemented");
+});
+//delete post
+exports.manufacturer_delete_post.asyncHandler(async(req, res, next) =>{
+  res.send("not implemented");
+});
+//update get
+exports.manufacturer_update_get.asyncHandler(async(req, res, next) =>{
+  res.send("not implemented");
+});
+//update post
+exports.manufacturer_update_post.asyncHandler(async(req, res, next) =>{
+  res.send("not implemented");
+});
