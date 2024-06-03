7c64a5d2abdf7e8f0c18481d725e6e1c2a812ba7
John O'Brien <johnobrien.dev@gmail.com>
2024-06-03 09:02:59 -0400

    create manufacturer create form

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,6 +1,9 @@
 const Manufacturer = require("../models/manufacturer");
 const Disc = require("../models/disc");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+
 // Display list of all Manufacturers.
 exports.manufacturer_list = asyncHandler(async (req, res, next) => {
   const allManufacturers = await Manufacturer.find().exec();
@@ -33,12 +36,40 @@ exports.manufacturer_detail = asyncHandler(async (req, res, next) => {
 });
 //create get
 exports.manufacturer_create_get = asyncHandler(async(req, res, next) =>{
-  res.send("not implemented");
+  res.render("manufacturer_form", {title: "Add a Manufacturer"});
 });
 // create post
-exports.manufacturer_create_post = asyncHandler(async(req, res, next) =>{
-  res.send("not implemented");
-});
+exports.manufacturer_create_post = [
+  body("name").trim().isLength({min:1}).escape().withMessage("Specify a disc manufacturer"),
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data
+    const manufacturer = new Manufacturer({
+      name: req.body.name
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("manufacturer_form", {
+        title: "Create Manufacturer",
+        manufacturer: manufacturer,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Save author.
+      await manufacturer.save();
+      // Redirect to new author record.
+      res.redirect(manufacturer.url);
+    }
+  }),
+]
+ 
 //delete get
 exports.manufacturer_delete_get = asyncHandler(async(req, res, next) =>{
   res.send("not implemented");
